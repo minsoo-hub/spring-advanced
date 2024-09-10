3283757498c744931ba1e6dfe19f7d69260fb1bd
naminsu <naminsoo1020@naver.com>
2024-09-10 11:10:49 +0900

    레벨 1-1: Early Return

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -27,13 +27,19 @@ public class AuthService {
     @Transactional
     public SignupResponse signup(SignupRequest signupRequest) {
 
-        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
-
-        UserRole userRole = UserRole.of(signupRequest.getUserRole());
+        if (signupRequest.getEmail() == null) {
+            throw new InvalidRequestException("이메일을 입력해주세요");
+        }
 
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
             throw new InvalidRequestException("이미 존재하는 이메일입니다.");
         }
+
+        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+
+        UserRole userRole = UserRole.of(signupRequest.getUserRole());
+
+
 
         User newUser = new User(
                 signupRequest.getEmail(),
